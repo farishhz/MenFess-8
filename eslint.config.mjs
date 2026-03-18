cfff6f6b4ad49873e1046464ac4d013f8ccb7272
Alfarisi Azmir <alfarisiazmir14@gmail.com>
2026-03-18 19:31:03 +0700

    Remove extra blank lines in eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,8 @@ const compat = new FlatCompat({
   // import.meta.dirname is available after Node.js v20.11.0
   baseDirectory: import.meta.dirname,
 })
- 
+
+
 const eslintConfig = [
   ...compat.config({
     extends: ['next'],
